6f30f624f1af6f233675091a10d78792adfd50ac
Semyon Denisov <91726358+SemyonDenisov@users.noreply.github.com>
2025-05-22 10:26:33 +0400

    Make exec jar (#6)
    * make Executable jar and
    
    * small typos in test
    
    * update README
    
    * update README
    
    * update README
    
    * update README
    
    * update README

--- src/main/java/ru/yandex/DAO/comment/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.DAO.comment;
-
-public interface CommentRepository {
-    void editComment(int commentId, String text);
-
-    void createComment(int postId, String text);
-
-    void deleteCommentById(int commentId);
-}
--- src/main/java/ru/yandex/service/comment/CommentService.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.service.comment;
-
-public interface CommentService {
-    void editComment(int commentId, String text);
-
-    void createComment(int postId, String text);
-
-    void deleteComment(int commentId);
-}
